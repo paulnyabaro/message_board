15c69df94bab376a3116c8b936adfbac8c0b6b8d
paulnyabaro <paulnyabaro1@gmail.com>
2023-02-01 05:27:56 +0300

    Added page tests

--- posts/tests.py
+++ posts/tests.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from django.urls import reverse
 from .models import Post
 
 
@@ -9,3 +10,19 @@ class PostTests(TestCase):
 
     def test_model_content(self):
         self.assertEqual(self.post.text, 'This is a test')
+
+    def test_url_exists_at_correct_location(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_url_available_by_name(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_template_name_correct(self):
+        response = self.client.get(reverse('home'))
+        self.assertTemplateUsed(response, 'home.html')
+    
+    def test_temple_content(self):
+        response = self.client.get(reverse('home'))
+        self.assertContains(response, 'This is a test')
